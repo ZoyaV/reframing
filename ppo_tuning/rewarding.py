c0757183fc036c346669e5a54553012f3ebb9535
Зоя Воловикова <z.volovikova@petrosoft.su>
2023-07-26 16:37:47 +0300

    new rewards

--- ppo_tuning/rewarding.py
+++ ppo_tuning/rewarding.py
@@ -1,23 +1,27 @@
 from metrics.metrics import calculate_iou
 from detectors.owlvit import OwlViTDetector
 
+import re
+
 
 def reinforce_loss(logits, labels, model, image):
     preds = logits
     metrics = []
+    stopwords = ['im', 'stop', 'the', 'to', 'and', 'a', 'in', 'it', 'is', 'i', 'that', 'had', 'on', 'for', 'were', 'was']
+    t = 0
     for i in range(len(logits)):
-
-        pred = preds[i].split("+")[0]
-
-        print("!!!!!!!!!!!!!!!!1")
-        print(preds[i])
-        print(pred)
-        print("!!!!!!!!!!!!!!!!1")
-
+        pred = preds[i].lower().replace("<pad>", "").split("</s>")
+        pred = pred if pred not in stopwords else "word"
+        pred = pred[0].split()[0]
+        regex = re.compile('[^a-zA-Z]')
+        pred = regex.sub('', pred)
         try:
             true_bbox = eval(labels[i])
             pred_bbox = (model.get_bboxes(image[i], pred)[0][1]).tolist()
-            metrics.append(calculate_iou(true_bbox, pred_bbox))
-        except:
-            metrics.append(0.)
+            t += 0.3
+            t += calculate_iou(true_bbox, pred_bbox)*1.5
+        except Exception as e:
+            t = -0.1
+            print(e)
+        metrics.append(t)
     return metrics
