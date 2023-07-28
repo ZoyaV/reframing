273cb8b7629b719481aefadb7c4407c00ee9bff8
Зоя Воловикова <z.volovikova@petrosoft.su>
2023-07-28 15:41:52 +0300

    fix prompt

--- ppo_tuning/data_loaders.py
+++ ppo_tuning/data_loaders.py
@@ -62,7 +62,7 @@ class SegmentationsDataset(CustomDataset):
 class HFDataset(CustomDataset):
     def __init__(self, tokenizer, txt_in_len, inp_column, out_column):
         data = pd.read_csv('./dataset/hf_prompts.csv')
-        data['prompt'] = data['text'].map(lambda x: f"Fix sentence: '{x}'")
+        data['prompt'] = data['text'].map(lambda x: f"The fixed version of sentence: '{x}' is")
 
         super(HFDataset, self).__init__(data, tokenizer, txt_in_len, inp_column, out_column)
 
